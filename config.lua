7f999b6cbcbe9404637335f8b0a69d9214c5f74d
felixsnow <crimsonflying@outlook.com>
2022-06-28 02:12:40 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -25,6 +25,11 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- override a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
+-- cmp settings
+lvim.builtin.cmp.completion = {
+  completeopt = 'menu,menuone,noinsert' -- helight first selection
+}
+
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 -- local _, actions = pcall(require, "telescope.actions")
