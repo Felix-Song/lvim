bf0e86249ad10a6ae0a861dd3f69cccaf9ff8b11
felixsnow <crimsonflying@outlook.com>
2022-06-29 00:54:29 +0800

    ?

--- config.lua
+++ config.lua
@@ -12,7 +12,7 @@ vim.wo.relativenumber = true
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
-
+vim.cmd('set clipboard+=unnamedplus')
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
